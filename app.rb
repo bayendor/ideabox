5b94070bcd7177347f86c6a9c0c53893190e2c7e
David Bayendor <dbayendor@gmail.com>
2014-10-27 19:01:57 -0600

    I0: Getting Started

---
+++ app.rb
@@ -0,0 +1,7 @@
+# require 'sinatra/reloader'
+
+class IdeaBoxApp < Sinatra::Base
+  get '/' do
+    "<h1>Hello, World!</h1><blockquote>I guess I always felt even if the world came to an end, McDonald's would still be open. <cite>Susan Beth Pfeffer</cite></blockquote>"
+  end
+end
