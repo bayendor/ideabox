7553bcc4c9af0450a52e6db227b4960d70f74150
David Bayendor <dbayendor@gmail.com>
2014-10-27 20:04:23 -0600

    I3: Deleting Ideas

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 require './idea'
 
 class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
+
   configure :development do
     register Sinatra::Reloader
   end
+
 
   get '/' do
     erb :index, locals: {ideas: Idea.all}
@@ -16,6 +19,11 @@ class IdeaBoxApp < Sinatra::Base
   post '/' do
     idea = Idea.new(params['idea_title'], params['idea_description'])
     idea.save
+    redirect '/'
+  end
+
+  delete '/:id' do |id|
+    Idea.delete(id.to_i)
     redirect '/'
   end
 end
